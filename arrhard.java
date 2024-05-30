0555530f8ccaf0fb257796dcedfc5e4be2567a7d
sks032011 <skrs032011@gmail.com>
2024-05-30 11:25:55 +0530

    30.5

--- arrhard.java
+++ arrhard.java
@@ -310,50 +310,50 @@
 //MERGE OVERLAPPING 
 // BRUTE
 
-import java.util.*;
+// import java.util.*;
 
-public class arrayhard {
-    public static List<List<Integer>> mergeOverlappingIntervals(int[][] arr) {
-        int n = arr.length;
-        Arrays.sort(arr, new Comparator<int[]>() {
-            public int compare(int[] a, int[] b) {
-                return a[0] - b[0];
-            }
-        });
-        List<List<Integer>> ans = new ArrayList<>();
-        for (int i = 0; i < arr.length; i++) {
+// public class arrayhard {
+//     public static List<List<Integer>> mergeOverlappingIntervals(int[][] arr) {
+//         int n = arr.length;
+//         Arrays.sort(arr, new Comparator<int[]>() {
+//             public int compare(int[] a, int[] b) {
+//                 return a[0] - b[0];
+//             }
+//         });
+//         List<List<Integer>> ans = new ArrayList<>();
+//         for (int i = 0; i < arr.length; i++) {
 
-            int start = arr[i][0];
-            int end = arr[i][1];
-            // when iterating i skip all which are already mergerd by checking last elemt of
-            // prvs array and last elmt of current array (end)
-            if (!ans.isEmpty() && end <= ans.get(ans.size() - 1).get(1)) {
-                continue;
-            }
-            for (int j = i + 1; j < n; j++) {
-                if (arr[j][0] <= end) {
-                    end = Math.max(end, arr[j][1]);
-                } else {
-                    break; // nothing left to merge
-                }
-            }
-            ans.add(Arrays.asList(start, end));
+//             int start = arr[i][0];
+//             int end = arr[i][1];
+//             // when iterating i skip all which are already mergerd by checking last elemt of
+//             // prvs array and last elmt of current array (end)
+//             if (!ans.isEmpty() && end <= ans.get(ans.size() - 1).get(1)) {
+//                 continue;
+//             }
+//             for (int j = i + 1; j < n; j++) {
+//                 if (arr[j][0] <= end) {
+//                     end = Math.max(end, arr[j][1]);
+//                 } else {
+//                     break; // nothing left to merge
+//                 }
+//             }
+//             ans.add(Arrays.asList(start, end));
 
-        }
-        return ans;
-    }
+//         }
+//         return ans;
+//     }
 
-    public static void main(String[] args) {
-        int[][] arr = { { 1, 3 }, { 8, 10 }, { 2, 6 }, { 15, 18 } };
-        List<List<Integer>> ans = mergeOverlappingIntervals(arr);
-        System.out.print("The merged intervals are: \n");
-        for (List<Integer> it : ans) {
-            System.out.print("[" + it.get(0) + ", " + it.get(1) + "] ");
-        }
-        System.out.println();
-    }
+//     public static void main(String[] args) {
+//         int[][] arr = { { 1, 3 }, { 8, 10 }, { 2, 6 }, { 15, 18 } };
+//         List<List<Integer>> ans = mergeOverlappingIntervals(arr);
+//         System.out.print("The merged intervals are: \n");
+//         for (List<Integer> it : ans) {
+//             System.out.print("[" + it.get(0) + ", " + it.get(1) + "] ");
+//         }
+//         System.out.println();
+//     }
 
-}
+// }
 
 // new Comparator<int[]>() is a way to create an instance of a comparator for
 // arrays of integers (int[]).
@@ -394,7 +394,7 @@ public class arrayhard {
 //         //https://chat.openai.com/share/7141a35e-0328-4c2f-857c-5c8e02c7567e
 //     }
 // }
-//tuff code
+
 
 
 
@@ -441,3 +441,259 @@ public class arrayhard {
 // }
 
 // ---------------------------------------------------------------------------
+
+// Merge two Sorted Arrays Without Extra Space
+
+// OPTIMAL 1 
+
+// import java.util.*;
+
+// public class tUf {
+
+//     public static void merge(long[] arr1, long[] arr2, int n, int m) {
+
+//         // Declare 2 pointers:
+//         int left = n - 1;
+//         int right = 0;
+
+//         // Swap the elements until arr1[left] is
+//         // smaller than arr2[right]:
+//         while (left >= 0 && right < m) {
+//             if (arr1[left] > arr2[right]) {
+//                 long temp = arr1[left];
+//                 arr1[left] = arr2[right];
+//                 arr2[right] = temp;
+//                 left--;
+//                 right++;
+//             } else {
+//                 break;
+//             }
+//         }
+
+//         // Sort arr1[] and arr2[] individually:
+//         Arrays.sort(arr1);
+//         Arrays.sort(arr2);
+//     }
+
+//     public static void main(String[] args) {
+//         long[] arr1 = {1, 4, 8, 10};
+//         long[] arr2 = {2, 3, 9};
+//         int n = 4, m = 3;
+//         merge(arr1, arr2, n, m);
+//         System.out.println("The merged arrays are:");
+//         System.out.print("arr1[] = ");
+//         for (int i = 0; i < n; i++) {
+//             System.out.print(arr1[i] + " ");
+//         }
+//         System.out.print("\narr2[] = ");
+//         for (int i = 0; i < m; i++) {
+//             System.out.print(arr2[i] + " ");
+//         }
+//         System.out.println();
+//     }
+
+// }
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// OPTIMAL 2 SHELL Sort
+// class Solution {
+//     public static void swapIfBigger(int[] arr1, int[] arr2, int ind1, int ind2) {
+//         if (arr1[ind1] > arr2[ind2]) {
+//             int temp = arr1[ind1];
+//             arr1[ind1] = arr2[ind2];
+//             arr2[ind2] = temp;
+//         }
+//     }
+
+//     public static void merge(int[] nums1, int m, int[] nums2, int n) {
+//         int len = m + n;
+//         // Initial gap:
+//         int gap = (len / 2) + (len % 2);
+
+//         while (gap > 0) {
+//             int l = 0;
+//             int r = l + gap;
+//             while (r < len) {
+//                 // case 1: l in nums1 and r in nums2
+//                 if (l < m && r >= m) {
+//                     swapIfBigger(nums1, nums2, l, r - m);
+//                 }
+//                 // case 2: both in nums1
+//                 else if (l < m && r < m) {
+//                     swapIfBigger(nums1, nums1, l, r);
+//                 }
+//                 // case 3: both in nums2
+//                 else if (l >= m) {
+//                     swapIfBigger(nums2, nums2, l - m, r - m);
+//                 }
+//                 // case 4: l in nums2 and r in nums2 (shouldn't happen in this context)
+//                 l++;
+//                 r++;
+//             }
+//             // break if iteration gap=1 is completed:
+//             if (gap == 1) break;
+
+//             // Otherwise, calculate new gap:
+//             gap = (gap / 2) + (gap % 2);
+//         }
+
+       
+//     }}
+// ++++++++++++++++++++++++++++++++++++
+// 0 CONDITION
+// class Solution {
+//     public void merge(int[] nums1, int m, int[] nums2, int n) {
+//         int i=m-1;int j=n-1;int k=m+n-1;
+//         while(j>=0){
+//             if(i>=0 && nums1[i]>nums2[j]){
+//                 nums1[k]=nums1[i];
+//                 k--;i--;
+//             }
+//             else{
+//                 nums1[k]=nums2[j];
+//                 k--;j--;
+//             }
+//         }
+//     }
+// }
+// =------------------------------------------------------------
+// GFG
+// Find the repeating and missing numbers
+// BETTER 
+
+// public class arrayhard {
+// public static int[] findMissingRepeatingNumbers(int[]arr){
+//     int n=arr.length;
+//     int[] hash = new int[n + 1]; // hash array
+
+// for (int i = 0; i <n; i++) {
+//     hash[arr[i]]++;
+
+// }
+// int repeating = -1, missing = -1;
+// for (int i = 1; i <= n; i++) {
+//     if (hash[i] == 2) repeating = i;
+//     else if (hash[i] == 0) missing = i;
+
+//     if (repeating != -1 && missing != -1)
+//         break;
+// }
+// int[] ans = {repeating, missing};
+// return ans;
+// }
+//     public static void main(String[] args) {
+//         int[] a = {3, 1, 2, 5, 4, 6, 7, 5};
+//         int[] ans = findMissingRepeatingNumbers(a);
+//         System.out.println("The repeating and missing numbers are: {"
+//                            + ans[0] + ", " + ans[1] + "}");
+//     }
+// }
+
+// ++++++++++++++++++++++++++++++++
+// oPTIMAL 
+// class Solve {
+//     int[] findTwoElement(int arr[], int N) {
+//         // Sum of first N natural numbers
+//         long SN = (N * (N + 1)) / 2;
+//         // Sum of squares of first N natural numbers
+//         long S2N = (N * (N + 1) * (2 * N + 1)) / 6;
+       
+//         long S = 0, S2 = 0;
+//         for (int i = 0; i < N; i++) {
+//             S += arr[i];
+//             S2 += (long)arr[i] * (long)arr[i];
+//         }
+        
+//         // Equation 1: x - y = S - SN
+//         long val1 = S - SN;
+//         // Equation 2: x^2 - y^2 = S2 - S2N
+//         long val2 = S2 - S2N;
+        
+//         // x^2 - y^2 = (x - y)(x + y)
+//         // val2 = val1 * (x + y)
+//         long val3 = val2 / val1;
+        
+//         // Solve the two equations:
+//         // x - y = val1
+//         // x + y = val3
+//         long x = (val1 + val3) / 2;
+//         long y = x - val1;
+        
+//         int[] ans = {(int)x, (int)y};
+//         return ans;
+//     }
+// }
+// -----------------------------------------------------------------
+
+// COUNT INVERSION 
+
+// import java.util.*;
+
+// public class arrhard {
+
+//     private static int merge(int[] arr, int low, int mid, int high) {
+//         ArrayList<Integer> temp = new ArrayList<>(); // temporary array
+//         int left = low;      // starting index of left half of arr
+//         int right = mid + 1;   // starting index of right half of arr
+
+//         //Modification 1: cnt variable to count the pairs:
+//         int cnt = 0;
+
+//         //storing elements in the temporary array in a sorted manner//
+
+//         while (left <= mid && right <= high) {
+//             if (arr[left] <= arr[right]) {
+//                 temp.add(arr[left]);
+//                 left++;
+//             } else {
+//                 temp.add(arr[right]);
+//                 cnt += (mid - left + 1); //Modification 2
+//                 right++;
+//             }
+//         }
+
+//         // if elements on the left half are still left //
+
+//         while (left <= mid) {
+//             temp.add(arr[left]);
+//             left++;
+//         }
+
+//         //  if elements on the right half are still left //
+//         while (right <= high) {
+//             temp.add(arr[right]);
+//             right++;
+//         }
+
+//         // transfering all elements from temporary to arr //
+//         for (int i = low; i <= high; i++) {
+//             arr[i] = temp.get(i - low);
+//         }
+//         return cnt; // Modification 3
+//     }
+
+//     public static int mergeSort(int[] arr, int low, int high) {
+//         int cnt = 0;
+//         if (low >= high) return cnt;
+//         int mid = (low + high) / 2 ;
+//         cnt += mergeSort(arr, low, mid);  // left half
+//         cnt += mergeSort(arr, mid + 1, high); // right half
+//         cnt += merge(arr, low, mid, high);  // merging sorted halves
+//         return cnt;
+//     }
+
+//     public static int numberOfInversions(int[] a, int n) {
+//         // Count the number of pairs:
+//         return mergeSort(a, 0, n - 1);
+//     }
+
+
+//     public static void main(String[] args) {
+//         int[] a = {5, 4, 3, 2, 1};
+//         int n = 5;
+//         int cnt = numberOfInversions(a, n);
+//         System.out.println("The number of inversions are: " + cnt);
+//     }
+// }
+
--- recursion.java
+++ recursion.java
@@ -559,53 +559,73 @@
 // ----------------------------------------------------------------------------------
 
 //Merge Sort 
+// import java.util.*;
 
-// public class recursion {
-//     public static void divide(int arr[], int low, int high) {
-//         if (low >= high) {
-//             return;
-//         }
-//         int mid = low + (high - low) / 2;
-//         divide(arr, low, mid);
-//         divide(arr, mid + 1, high);
-//         merge(arr, low, mid, high);
-//     }
+// class Solution {
+//     private static void merge(int[] arr, int low, int mid, int high) {
+//         ArrayList<Integer> temp = new ArrayList<>(); // temporary array
+//         int left = low;      // starting index of left half of arr
+//         int right = mid + 1;   // starting index of right half of arr
 
-//     public static void merge(int arr[], int low, int mid, int high) {
-//         int temp[] = new int[high - low + 1];
-//         int left = low;
-//         int right = mid + 1;
-//         int x = 0;
+//         //storing elements in the temporary array in a sorted manner//
+
 //         while (left <= mid && right <= high) {
 //             if (arr[left] <= arr[right]) {
-//                 temp[x++] = arr[left++];
+//                 temp.add(arr[left]);
+//                 left++;
 //             } else {
-//                 temp[x++] = arr[right++];
+//                 temp.add(arr[right]);
+//                 right++;
 //             }
+//         }
 
-//         }
+//         // if elements on the left half are still left //
+
 //         while (left <= mid) {
-//             temp[x++] = arr[left++];
+//             temp.add(arr[left]);
+//             left++;
 //         }
+
+//         //  if elements on the right half are still left //
 //         while (right <= high) {
-//             temp[x++] = arr[right++];
+//             temp.add(arr[right]);
+//             right++;
 //         }
-//         for (int i = 0, j = low; i < temp.length; i++, j++) {
-//             arr[j] = temp[i];
+
+//         // transfering all elements from temporary to arr //
+//         for (int i = low; i <= high; i++) {
+//             arr[i] = temp.get(i - low);
 //         }
 //     }
 
-//     public static void main(String[] args) {
-//         int arr[] = { 1, 5, 4, 8, 9, 6, 2, 3, 7 };
-//         int n = arr.length;
-//         divide(arr, 0, n - 1);
+//     public static void mergeSort(int[] arr, int low, int high) {
+//         if (low >= high) return;
+//         int mid = (low + high) / 2 ;
+//         mergeSort(arr, low, mid);  // left half
+//         mergeSort(arr, mid + 1, high); // right half
+//         merge(arr, low, mid, high);  // merging sorted halves
+//     }
+// }
+// public class tUf {
+//     public static void main(String args[]) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = 7;
+//         int arr[] = { 9, 4, 7, 6, 3, 1, 5 };
+//         System.out.println("Before sorting array: ");
 //         for (int i = 0; i < n; i++) {
-
+//             System.out.print(arr[i] + " ");
+//         }
+//         System.out.println();
+//         Solution.mergeSort(arr, 0, n - 1);
+//         System.out.println("After sorting array: ");
+//         for (int i = 0; i < n; i++) {
 //             System.out.print(arr[i] + " ");
 //         }
 //         System.out.println();
 //     }
+
 // }
+
 
 // ---------------------------------------------------------------------------------------------------
 
